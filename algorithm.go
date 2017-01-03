483ffca83e5be72a81b76776c8431d78479664bf
Ryan Carrier <ryan.j.carrier@gmail.com>
2017-01-03 17:33:54 +1000

    Public fields

--- algorithm.go
+++ algorithm.go
@@ -1,41 +1,40 @@
-
 package dijkstra
 
 func (g *Graph) ShortestPath(src, dest int) (x int) {
-    g.visit(src)
-    v := g.vertices[src]
-    h := make(Candidates, len(v.arcs))
-    // initialize the heap with out edges from src
-    for id, y := range v.arcs {
-        v := g.vertices[id]
-        // update the vertices being pointed to with the distance.
-        v.dist = y
-        g.vertices[id] = v
-        h.Push(v)
-    }
-    for src != dest {
-        if h.IsEmpty() {
-            return 1000000
-        }
-        v = h.Pop()
-        src = v.id
-        if g.visited[src] {
-            continue
-        }
-        g.visit(src)
-        for w, d := range v.arcs {
-            if g.visited[w] {
-                continue
-            }
-            c := g.vertices[w]
-            distance := d + v.dist
-            if distance < c.dist {
-                c.dist = distance
-                g.vertices[w] = c
-            }
-            h.Push(c)
-        }
-    }
-    v = g.vertices[dest]
-    return v.dist
+	g.visit(src)
+	v := g.vertices[src]
+	h := make(Candidates, len(v.Arcs))
+	// initialize the heap with out edges from src
+	for id, y := range v.Arcs {
+		v := g.vertices[id]
+		// update the vertices being pointed to with the distance.
+		v.Dist = y
+		g.vertices[id] = v
+		h.Push(v)
+	}
+	for src != dest {
+		if h.IsEmpty() {
+			return 1000000
+		}
+		v = h.Pop()
+		src = v.ID
+		if g.visited[src] {
+			continue
+		}
+		g.visit(src)
+		for w, d := range v.Arcs {
+			if g.visited[w] {
+				continue
+			}
+			c := g.vertices[w]
+			distance := d + v.Dist
+			if distance < c.Dist {
+				c.Dist = distance
+				g.vertices[w] = c
+			}
+			h.Push(c)
+		}
+	}
+	v = g.vertices[dest]
+	return v.Dist
 }
--- graph_test.go
+++ graph_test.go
@@ -1,98 +1,97 @@
-
 package dijkstra
 
-import(
-    "testing"
+import (
+	"testing"
 )
 
 func AssertEqual(a, b interface{}, t *testing.T) {
-    if a != b {
-        t.Logf("%v == %v", a, b)
-        t.Fail()
-    }
+	if a != b {
+		t.Logf("%v == %v", a, b)
+		t.Fail()
+	}
 }
 
 func TestAssertedLengthofDataFile(t *testing.T) {
-    const expected = 200
-    g := NewGraphFromFile("dijkstraData.txt")
-    result := g.Len()
-    AssertEqual(expected, result, t)
+	const expected = 200
+	g := NewGraphFromFile("dijkstraData.txt")
+	result := g.Len()
+	AssertEqual(expected, result, t)
 }
 
 func TestShortestPathIsLowestSingleArc(t *testing.T) {
-    const expected = 15
-    v := map[int]Vertex{
-        1 : {
-            id: 1,
-            arcs: map[int]int{
-                2: 14,
-                3: 15,
-            },
-         },
-         2: {
-             id : 2,
-             arcs: map[int]int{
-                 3: 8,
-             },
-         },
-         3: {
-             id: 3,
-             arcs: map[int]int{},
-         },
-     }
-    g := NewGraph(v)
-    result := g.ShortestPath(1, 3)
-    AssertEqual(result, expected, t)
-    if t.Failed() {
-        t.Log(g)
-    }
+	const expected = 15
+	v := map[int]Vertex{
+		1: {
+			ID: 1,
+			Arcs: map[int]int{
+				2: 14,
+				3: 15,
+			},
+		},
+		2: {
+			ID: 2,
+			Arcs: map[int]int{
+				3: 8,
+			},
+		},
+		3: {
+			ID:   3,
+			Arcs: map[int]int{},
+		},
+	}
+	g := NewGraph(v)
+	result := g.ShortestPath(1, 3)
+	AssertEqual(result, expected, t)
+	if t.Failed() {
+		t.Log(g)
+	}
 }
 
 func TestShortestTakesAHop(t *testing.T) {
-    const expected = 10
-    v := map[int]Vertex{
-        1 : {
-            id: 1,
-            arcs: map[int]int{
-                2: 2,
-                3: 15,
-            },
-         },
-         2: {
-             id : 2,
-             arcs: map[int]int{
-                 3: 8,
-             },
-         },
-         3: {
-             id: 3,
-             arcs: map[int]int{},
-         },
-     }
-    g := NewGraph(v)
-    result := g.ShortestPath(1, 3)
-    AssertEqual(result, expected, t)
-    if t.Failed() {
-        t.Log(g)
-    }
+	const expected = 10
+	v := map[int]Vertex{
+		1: {
+			ID: 1,
+			Arcs: map[int]int{
+				2: 2,
+				3: 15,
+			},
+		},
+		2: {
+			ID: 2,
+			Arcs: map[int]int{
+				3: 8,
+			},
+		},
+		3: {
+			ID:   3,
+			Arcs: map[int]int{},
+		},
+	}
+	g := NewGraph(v)
+	result := g.ShortestPath(1, 3)
+	AssertEqual(result, expected, t)
+	if t.Failed() {
+		t.Log(g)
+	}
 }
 
 func TestUnnonnectedNodeReturnsMillion(t *testing.T) {
-    const expected = 1000000
-    v := map[int]Vertex{
-        1: {
-            id: 1,
-            arcs: map[int]int{},
-        },
-        2: {
-            id: 2,
-            arcs: map[int]int{},
-        },
-    }
-    g := NewGraph(v)
-    result := g.ShortestPath(1, 3)
-    AssertEqual(result, expected, t)
-    if t.Failed() {
-        t.Log(g)
-    }
+	const expected = 1000000
+	v := map[int]Vertex{
+		1: {
+			ID:   1,
+			Arcs: map[int]int{},
+		},
+		2: {
+			ID:   2,
+			Arcs: map[int]int{},
+		},
+	}
+	g := NewGraph(v)
+	result := g.ShortestPath(1, 3)
+	AssertEqual(result, expected, t)
+	if t.Failed() {
+		t.Log(g)
+	}
 }
